defe3c8f173f45523053158511c8562b9d6290d0
Kevin Wuerdeman <kevinwuerdeman@gmail.com>
2018-01-16 13:43:23 -0600

    form adds tweets

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
 
@@ -13,14 +14,24 @@ router.use(express.static('public'));
 router.get('/users/:name', function(req, res, next) {
   const name = req.params.name;
   let list = tweetBank.find( {name: name} );
-  res.render('index', {tweets: list, showForm: true});
+  res.render('index', {tweets: list, showForm: true, name: name});
 });
 
 router.get('/tweets/:id', function(req, res, next) {
   const id = req.params.id;
   let tweet = tweetBank.find( {id: id} );
-  res.render('index', {tweets: tweet, showForm: true} );
+  res.render('index', {tweets: tweet} );
 });
+
+const urlencodedParser = bodyParser.urlencoded();
+
+router.post('/tweets', urlencodedParser, function(req, res) {
+  if (!req.body) return res.sendStatus(400);
+  const name = req.body.name;
+  const text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
+})
 
 // router.get('/stylesheets/:file', (req, res, next) => {
 
