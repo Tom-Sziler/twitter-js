9884afba7e8ef181ad08b9ed4182159ab3c77d5f
Tom Sziler <tom.sziler@gmail.com>
2018-01-16 09:52:22 -0600

    more progress

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const app = express();
+const routes = require('./routes');
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
 
+app.use('/', routes);
+
+// app.use((req, res, next) =>{
+//   console.log(req.method, req.path);
+//   next();
+// });
 
 
-app.use((req, res, next) =>{
-  console.log(req.method, req.path);
-  next();
-});
-
-app.get('/', (req, res, next) => {
-  res.render('index', {title: 'Hall of Fame', people: people} );
-});
 
 
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
 
 
 
